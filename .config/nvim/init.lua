413f3c7fc2afc1c9b44ed896450e25a5e4402e42
Marcelo Alves <marcelo.alves.design@outlook.com>
2026-01-04 21:06:45 +0900

    feat: add uv and ty for python

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -85,4 +85,4 @@ vim.g.slime_bracketed_paste = 1
 vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
 vim.g.slime_target = "tmux"
 
-vim.lsp.enable({ "tsgo" })
+vim.lsp.enable({ "tsgo", "ty" })
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # dotfiles
 
-
 ```sh
+# TypeScript
 npm install -g prettier @typescript/native-preview
+
+# Python
+curl -LsSf https://astral.sh/uv/install.sh | sh
+curl -LsSf https://astral.sh/ty/install.sh | sh
 ```
