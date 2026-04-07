ad83be1ab41f629a8758b69285bd206ff6ceaf26
Marcelo Alves <marcelo.alves.design@outlook.com>
2026-04-08 08:06:09 +0900

    feat: update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,28 +1,86 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.g.have_nerd_font = false
 
-vim.o.breakindent = true
+-- Make line numbers default
+vim.o.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.o.relativenumber = true
+
+-- Sync clipboard between OS and Neovim
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
 vim.schedule(function()
   vim.o.clipboard = "unnamedplus"
 end)
-vim.o.cursorline = true
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
-vim.o.inccommand = "split"
-vim.o.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.scrolloff = 10
-vim.o.signcolumn = "yes"
 vim.o.smartcase = true
-vim.o.splitbelow = true
-vim.o.splitright = true
-vim.o.timeoutlen = 300
-vim.o.undofile = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = "yes"
+
+-- Decrease update time
 vim.o.updatetime = 250
 
-vim.opt.rtp:append("/opt/homebrew/opt/fzf")
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-guide-options`
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = "split"
+
+-- Show which line your cursor in on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Diagnostic Config & Keymaps
+--  See `:help 'vim.diagnostic.Opts'`
+vim.diagnostic.config({
+  update_in_insert = false,
+  severity_sort = true,
+  float = { border = "rounded", source = "if_many" },
+  underline = { severity = { min = vim.diagnostic.severity.WARN } },
+
+  -- Can switch between these as you prefer
+  virtual_text = true, -- Text shows up at the end of the line
+  virtual_lines = false, -- Text shows up underneath the line, with virtual lines
+
+  -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+  jump = { float = true },
+})
+
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+vim.opt.rtp:append("/opt/homebrew/bin/fzf")
 
 vim.keymap.set("n", "<leader>fzf", "<cmd>:FZF<CR>")
 
@@ -32,8 +90,6 @@ if vim.fn.executable("rg") == 1 then
 end
 
 vim.cmd("set completeopt+=noselect")
-
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ev)
@@ -52,10 +108,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
-vim.diagnostic.config({
-  virtual_text = { current_line = true },
-})
-
 vim.pack.add({
   {
     src = "https://github.com/stevearc/conform.nvim",
@@ -64,6 +116,10 @@ vim.pack.add({
   {
     src = "https://github.com/jpalardy/vim-slime",
     name = "vim-slime",
+  },
+  {
+    src = "https://github.com/nvim-treesitter/nvim-treesitter",
+    name = "nvim-treesitter",
   },
 })
 
